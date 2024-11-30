48d33cb98ddfd021eccfa405929f000db6861f4b
Ismail <iboukhss.student@42luxembourg.lu>
2024-11-30 23:23:47 +0100

    feat: add cmd structure and cleanup headers

--- srcs/minishell.h
+++ srcs/minishell.h
@@ -1,25 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iboukhss <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/30 21:07:41 by iboukhss          #+#    #+#             */
+/*   Updated: 2024/11/30 23:19:28 by iboukhss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-#include "../libft/libft.h"
+# define WHITESPACE " \t\r\n\v"
+# define ACK_SYMBOLS "<|>"
+# define NAK_SYMBOLS "&/;"
 
-#define WHITESPACE " \t\r\n\v"
-#define ACK_SYMBOLS "<|>"
-#define NAK_SYMBOLS "&/;"
+# include <stdbool.h>
+# include <stddef.h>
 
-typedef struct  s_token t_token;
+// Forward declarations here
+typedef struct s_command	t_command;
+typedef struct s_token		t_token;
 
-typedef struct  s_token
+typedef struct s_command
+{
+	char		**args;		// Command and arguments
+	char		*infile;	// Input redirection file (<)
+	char		*outfile;	// Output redirection file (>)
+	bool		append;		// Flag for appending (>>)
+	char		*heredoc;	// Heredoc delimiter (<<)
+	t_command	*next;		// Pointer to next command in the pipeline
+}	t_command;
+
+typedef struct s_token
 {
 	char	*content;
-	char 	type;
-	t_token *next;
-}				t_token;
+	char	type;
+	t_token	*next;
+}	t_token;
 
 int		tokenize(char *line, t_token **token_list);
 void	add_back(t_token **token_list, t_token *new_token);
@@ -28,6 +48,5 @@ void	free_all(char *line, t_token *token_list);
 void	free_token_list(t_token *head);
 t_token	*get_token(char *line);
 t_token	*init_token(char *content, char type);
-
 
 #endif
