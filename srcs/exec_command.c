20cc1c7762c32c70e301ac2a7be493ef7747ed2d
Ismail <iboukhss.student@42luxembourg.lu>
2024-11-30 14:17:43 +0100

    fix: improve error checking

--- srcs/exec_command.c
+++ srcs/exec_command.c
@@ -6,7 +6,7 @@
 /*   By: iboukhss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 13:43:53 by iboukhss          #+#    #+#             */
-/*   Updated: 2024/11/29 18:33:17 by iboukhss         ###   ########.fr       */
+/*   Updated: 2024/11/30 14:12:00 by iboukhss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,23 +32,36 @@ void	exec_command(char *command, char **argv, char **envp)
 		exit(EXIT_FAILURE);
 	}
 	path = getenv("PATH");
+	if (path == NULL)
+	{
+		fprintf(stderr, "PATH variable not set\n");
+		exit(EXIT_FAILURE);
+	}
 	char *beg = path;
 	while (1)
 	{
 		char *end = ft_strchrnul(beg, ':');
-		if (end - beg == 0)
+		if (end - beg > 0)
+		{
+			// Need enough space for '/' and '\0'
+			if ((end - beg) + ft_strlen(command) + 2 > PATH_MAX)
+			{
+				fprintf(stderr, "Path too long\n");
+				exit(EXIT_FAILURE);
+			}
+			ft_memcpy(full_path, beg, end - beg);
+			full_path[end - beg] = '/';
+			ft_strcpy(full_path + (end - beg) + 1, command);
+			if (access(full_path, X_OK) == 0)
+			{
+				execve(full_path, argv, envp);
+				perror("execve");
+				exit(EXIT_FAILURE);
+			}
+		}
+		if (*end == '\0')
 		{
 			fprintf(stderr, "%s: command not found\n", command);
-			exit(EXIT_FAILURE);
-		}
-		// Assuming no overflow on full_path (tofix)
-		ft_memcpy(full_path, beg, end - beg);
-		full_path[end - beg] = '/';
-		ft_memcpy(full_path + (end - beg) + 1, command, ft_strlen(command) + 1);
-		if (access(full_path, X_OK) == 0)
-		{
-			execve(full_path, argv, envp);
-			perror("execve");
 			exit(EXIT_FAILURE);
 		}
 		beg = end + 1;
