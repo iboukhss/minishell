f95515d08da396af3cac0c1df55e6d2c29f95747
Ismail <iboukhss.student@42luxembourg.lu>
2024-12-02 17:00:11 +0100

    feat: add xmalloc and cleanup libft headers

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,11 +6,13 @@
 /*   By: dheck <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 15:58:32 by dheck             #+#    #+#             */
-/*   Updated: 2024/03/15 13:31:36 by dheck            ###   ########.fr       */
+/*   Updated: 2024/12/01 18:59:02 by iboukhss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+#include <stdlib.h>
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
