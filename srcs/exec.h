297a285ed6fb78df2447f7e64cc50c0cd2ae8455
Ismail <iboukhss.student@42luxembourg.lu>
2024-12-03 16:46:08 +0100

    fix: update function type definitions

--- srcs/exec.h
+++ srcs/exec.h
@@ -6,14 +6,16 @@
 /*   By: iboukhss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 13:39:56 by iboukhss          #+#    #+#             */
-/*   Updated: 2024/11/29 18:24:29 by iboukhss         ###   ########.fr       */
+/*   Updated: 2024/12/02 17:10:00 by iboukhss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EXEC_H
 # define EXEC_H
 
+# include "minishell.h"
+
 // Add function prototypes here
-void	exec_command(char *command, char **argv, char **envp);
+void	exec_command(t_command *cmd, t_shell *shell);
 
 #endif
