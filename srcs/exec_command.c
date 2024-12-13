15a19826c46ef673af5e99104562c371de272b11
Ismail <iboukhss.student@42luxembourg.lu>
2024-12-13 14:45:17 +0100

    feat: add forking to exec_command and refactor

--- srcs/exec_command.c
+++ srcs/exec_command.c
@@ -6,7 +6,7 @@
 /*   By: iboukhss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 13:43:53 by iboukhss          #+#    #+#             */
-/*   Updated: 2024/12/02 20:22:46 by iboukhss         ###   ########.fr       */
+/*   Updated: 2024/12/13 14:07:39 by iboukhss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,55 +16,119 @@
 #include <linux/limits.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
-// DESC: Checks if a command is an absolute path and if so, executes it.
-// Otherwise, we look for the executable within the PATH environment variable.
-void	exec_command(t_command *cmd, t_shell *shell)
+static void	exit_shell(int status, t_shell *shell)
 {
-	char	*path;
-	char	full_path[PATH_MAX];
+	shell->exit_status = status;
+	exit(status);
+}
 
-	if (access(cmd->args[0], X_OK) == 0)
+static char	*get_env(const char *key, t_shell *shell)
+{
+	char	**envp;
+	int		key_len;
+	int		i;
+
+	envp = shell->envs;
+	key_len = ft_strlen(key);
+	i = 0;
+	while (envp[i] != NULL)
 	{
-		execve(cmd->args[0], cmd->args, shell->envs);
-		perror("execve");
-		exit(EXIT_FAILURE);
+		if (strncmp(envp[i], key, key_len) == 0 && envp[i][key_len] == '=')
+		{
+			return (envp[i] + key_len + 1);
+		}
+		i++;
 	}
-	path = getenv("PATH");
-	if (path == NULL)
+	return (NULL);
+}
+
+static char	*resolve_path(const char *cmd_name, t_shell *shell)
+{
+	char	*path_var;
+	char	*cmd_path;
+	char	*dir_name;
+	char	*beg, *end;
+
+	if (access(cmd_name, X_OK) == 0)
+	{
+		return (ft_xstrdup(cmd_name));
+	}
+	path_var = get_env("PATH", shell);
+	if (path_var == NULL)
 	{
 		fprintf(stderr, "PATH variable not set\n");
-		exit(EXIT_FAILURE);
+		exit_shell(EXIT_FAILURE, shell);
 	}
-	char *beg = path;
-	while (1)
+	beg = path_var;
+	end = beg;
+	while (*end != '\0')
 	{
-		char *end = ft_strchrnul(beg, ':');
+		end = ft_strchrnul(beg, ':');
+		// Skip empty tokens
 		if (end - beg > 0)
 		{
-			// Need enough space for '/' and '\0'
-			if ((end - beg) + ft_strlen(cmd->args[0]) + 2 > PATH_MAX)
+			dir_name = strndup(beg, end - beg);
+			if (asprintf(&cmd_path, "%s/%s", dir_name, cmd_name) == -1)
 			{
-				fprintf(stderr, "Path too long\n");
-				exit(EXIT_FAILURE);
+				perror("asprintf");
+				free(dir_name);
+				return (NULL);
 			}
-			ft_memcpy(full_path, beg, end - beg);
-			full_path[end - beg] = '/';
-			ft_strcpy(full_path + (end - beg) + 1, cmd->args[0]);
-			if (access(full_path, X_OK) == 0)
+			free(dir_name);
+			if (access(cmd_path, X_OK) == 0)
 			{
-				execve(full_path, cmd->args, shell->envs);
-				perror("execve");
-				exit(EXIT_FAILURE);
+				return (cmd_path);
 			}
-		}
-		if (*end == '\0')
-		{
-			fprintf(stderr, "%s: command not found\n", cmd->args[0]);
-			exit(EXIT_FAILURE);
+			free(cmd_path);
 		}
 		beg = end + 1;
+	}
+	fprintf(stderr, "%s: command not found\n", cmd_name);
+	exit_shell(127, shell);
+	return (NULL);
+}
+
+void	exec_command(t_command *cmd, t_shell *shell)
+{
+	pid_t	pid;
+	int		status;
+	char	*cmd_path;
+
+	pid = fork();
+	if (pid < 0)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	// Child process
+	if (pid == 0)
+	{
+		cmd_path = resolve_path(cmd->args[0], shell);
+		execve(cmd_path, cmd->args, shell->envs);
+		perror("execve");
+		free(cmd_path);
+		exit_shell(EXIT_FAILURE, shell);
+	}
+	// Parent process
+	else
+	{
+		if (waitpid(pid, &status, 0) < 0)
+		{
+			perror("waitpid");
+			shell->exit_status = 1;
+		}
+		if (WIFEXITED(status))
+		{
+			shell->exit_status = WEXITSTATUS(status);
+		}
+		else
+		{
+			shell->exit_status = 1;
+		}
 	}
 }
 
