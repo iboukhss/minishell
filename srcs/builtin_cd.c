bc24e33ca217dc1663329e57fc3ee9b248614fa5
Ismail <iboukhss@student.42luxembourg.lu>
2024-12-18 18:25:16 +0100

    refactor: simplify cd builtin

--- srcs/builtin_cd.c
+++ srcs/builtin_cd.c
@@ -6,14 +6,13 @@
 /*   By: iboukhss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 13:44:17 by iboukhss          #+#    #+#             */
-/*   Updated: 2024/12/18 14:40:03 by iboukhss         ###   ########.fr       */
+/*   Updated: 2024/12/18 18:18:40 by iboukhss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "exec.h"
 #include "libft.h"
 
-#include <linux/limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -22,7 +21,6 @@ void	builtin_cd(t_command *cmd, t_shell *shell)
 {
 	int		argc;
 	char	*new_cwd;
-	char	old_cwd[PATH_MAX];
 
 	argc = ft_strlenv(cmd->args);
 	if (argc == 1)
@@ -30,7 +28,7 @@ void	builtin_cd(t_command *cmd, t_shell *shell)
 		new_cwd = get_env("HOME", shell);
 		if (new_cwd == NULL)
 		{
-			fprintf(stderr, "HOME variable not set\n");
+			fprintf(stderr, "cd: HOME not set\n");
 			exit(EXIT_FAILURE);
 		}
 		if (chdir(new_cwd) < 0)
@@ -41,11 +39,6 @@ void	builtin_cd(t_command *cmd, t_shell *shell)
 	}
 	else if (argc == 2)
 	{
-		if (getcwd(old_cwd, PATH_MAX) == NULL)
-		{
-			perror("getcwd");
-			exit(EXIT_FAILURE);
-		}
 		new_cwd = cmd->args[1];
 		if (chdir(new_cwd) < 0)
 		{
