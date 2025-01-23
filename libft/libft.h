eb9b6486861a8a8ac17a6418e9a0c439fef9312e
Heck Dennis <heckdennis257@gmail.com>
2025-01-23 21:04:43 +0100

    feature: adding ft_itoa prototype

--- libft/libft.h
+++ libft/libft.h
@@ -22,6 +22,7 @@ char	*ft_strcpy(char *dst, const char *src);
 char	*ft_strdup(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_itoa(int n);
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
