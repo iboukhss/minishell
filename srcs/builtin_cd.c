145549ab06c387ba0b03ae4b8b6d6d91de4b2db2
Ismail <iboukhss.student@42luxembourg.lu>
2025-02-04 20:37:07 +0100

    fix: set PWD and OLDPWD variables inside cd

--- srcs/builtin_cd.c
+++ srcs/builtin_cd.c
@@ -6,50 +6,59 @@
 /*   By: iboukhss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 13:44:17 by iboukhss          #+#    #+#             */
-/*   Updated: 2025/02/04 12:02:59 by iboukhss         ###   ########.fr       */
+/*   Updated: 2025/02/04 20:33:29 by iboukhss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "exec.h"
 
 #include "libft.h"
+#include <linux/limits.h>
 #include <stdio.h>
 #include <unistd.h>
 
 int	builtin_cd(t_command *cmd, t_shell *shell)
 {
-	int		argc;
-	char	*new_cwd;
+	int			argc;
+	const char	*target;
+	char		old_cwd[PATH_MAX];
+	char		new_cwd[PATH_MAX];
 
 	argc = ft_strv_length(cmd->args);
 	if (argc == 1)
 	{
-		new_cwd = get_env("HOME", shell);
-		if (new_cwd == NULL)
+		target = get_env("HOME", shell);
+		if (target == NULL)
 		{
 			log_error("cd: HOME not set");
 			return (MS_XFAILURE);
 		}
-		if (chdir(new_cwd) == -1)
-		{
-			perror("cd: chdir");
-			return (MS_XFAILURE);
-		}
-		return (MS_XSUCCESS);
 	}
 	else if (argc == 2)
 	{
-		new_cwd = cmd->args[1];
-		if (chdir(new_cwd) == -1)
-		{
-			perror("cd: chdir");
-			return (MS_XFAILURE);
-		}
-		return (MS_XSUCCESS);
+		target = cmd->args[1];
 	}
 	else
 	{
 		log_error("cd: too many arguments");
 		return (MS_XBADUSAGE);
 	}
+	if (getcwd(old_cwd, sizeof(old_cwd)) == NULL)
+	{
+		perror("cd: getcwd");
+		return (MS_XFAILURE);
+	}
+	if (chdir(target) == -1)
+	{
+		perror("cd: chdir");
+		return (MS_XFAILURE);
+	}
+	if (getcwd(new_cwd, sizeof(new_cwd)) == NULL)
+	{
+		perror("cd: getcwd");
+		return (MS_XFAILURE);
+	}
+	set_env("OLDPWD", old_cwd, shell);
+	set_env("PWD", new_cwd, shell);
+	return (MS_XSUCCESS);
 }
