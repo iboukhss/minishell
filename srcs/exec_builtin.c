87bf51572151c05d701da906c190c0e104e71526
Ismail <iboukhss@student.42luxembourg.lu>
2025-02-11 18:24:35 +0100

    norm: fix line count

--- srcs/exec_builtin.c
+++ srcs/exec_builtin.c
@@ -6,7 +6,7 @@
 /*   By: iboukhss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 15:20:16 by iboukhss          #+#    #+#             */
-/*   Updated: 2025/02/11 15:54:24 by iboukhss         ###   ########.fr       */
+/*   Updated: 2025/02/11 17:54:11 by iboukhss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,36 +16,23 @@
 
 /*
  * Finds the proper shell builtin.
+ * Last condition should is never reached.
  */
 int	exec_builtin(t_command *cmd, t_shell *shell)
 {
 	if (ft_strcmp(cmd->args[0], "env") == 0)
-	{
 		return (builtin_env(cmd, shell));
-	}
 	else if (ft_strcmp(cmd->args[0], "unset") == 0)
-	{
 		return (builtin_unset(cmd, shell));
-	}
 	else if (ft_strcmp(cmd->args[0], "export") == 0)
-	{
 		return (builtin_export(cmd, shell));
-	}
 	else if (ft_strcmp(cmd->args[0], "exit") == 0)
-	{
 		return (builtin_exit(cmd, shell));
-	}
 	else if (ft_strcmp(cmd->args[0], "echo") == 0)
-	{
 		return (builtin_echo(cmd));
-	}
 	else if (ft_strcmp(cmd->args[0], "pwd") == 0)
-	{
 		return (builtin_pwd(cmd));
-	}
 	else if (ft_strcmp(cmd->args[0], "cd") == 0)
-	{
 		return (builtin_cd(cmd, shell));
-	}
 	return (MS_XFAILURE);
 }
--- srcs/exec_pipeline.c
+++ srcs/exec_pipeline.c
@@ -6,7 +6,7 @@
 /*   By: iboukhss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 15:20:31 by iboukhss          #+#    #+#             */
-/*   Updated: 2025/02/11 15:43:50 by iboukhss         ###   ########.fr       */
+/*   Updated: 2025/02/11 18:17:05 by iboukhss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-static void exec_from_pipeline(t_command *cmd, t_shell *shell)
+static void	exec_from_pipeline(t_command *cmd, t_shell *shell)
 {
 	redirect_io(cmd, shell);
 	if (cmd->is_builtin)
@@ -29,6 +29,21 @@ static void exec_from_pipeline(t_command *cmd, t_shell *shell)
 	}
 }
 
+static void	handle_child(int prev_fd, int pfd[2], t_command *cmd, t_shell *sh)
+{
+	if (prev_fd != -1)
+	{
+		ft_xdup2(prev_fd, STDIN_FILENO);
+		close(prev_fd);
+	}
+	if (cmd->next)
+	{
+		close(pfd[0]);
+		ft_xdup2(pfd[1], STDOUT_FILENO);
+	}
+	exec_from_pipeline(cmd, sh);
+}
+
 int	exec_pipeline(t_command *cmd, t_shell *shell)
 {
 	int		prev_fd;
@@ -39,29 +54,12 @@ int	exec_pipeline(t_command *cmd, t_shell *shell)
 	while (cmd)
 	{
 		if (cmd->next)
-		{
 			ft_xpipe(pipefd);
-		}
 		pid = ft_xfork();
 		if (pid == 0)
-		{
-			if (prev_fd != -1)
-			{
-				ft_xdup2(prev_fd, STDIN_FILENO);
-				close(prev_fd);
-			}
-			if (cmd->next)
-			{
-				close(pipefd[0]);
-				ft_xdup2(pipefd[1], STDOUT_FILENO);
-				close(pipefd[1]);
-			}
-			exec_from_pipeline(cmd, shell);
-		}
+			handle_child(prev_fd, pipefd, cmd, shell);
 		if (prev_fd != -1)
-		{
 			close(prev_fd);
-		}
 		if (cmd->next)
 		{
 			close(pipefd[1]);
