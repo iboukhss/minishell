52e16a9f0182ba6d9a279329370650e0eb583ca6
Dennis Heck <heckdennis257@gmail.com>
2025-02-11 18:39:10 +0100

    testing: adding tests

--- mini_tester/tests/pipe.sh
+++ mini_tester/tests/pipe.sh
@@ -4,5 +4,8 @@ echo "hello world" | wc -l
 printf "Line 1\nLine 2\nLine 3\n" | wc -l
 echo "Home directory: $HOME" | grep "Home directory"
 env | grep "PATH"
-echo $(echo "hello world") | wc -w
 bash -c "echo 'Hello from subshell'" | grep 'Hello'
+env | sort | grep -v SHLVL | grep -v ^_
+cat ./test_files/infile_big | grep oi
+cat minishell.h | grep ");"$
+export GHOST=123 | env | grep GHOST
