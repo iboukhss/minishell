3605aedb55cdd5e7807b54733322c82fbabcee5b
Dennis Heck <heckdennis257@gmail.com>
2025-02-11 14:17:31 +0100

    testing: adding test cases

--- mini_tester/tests/bin.sh
+++ mini_tester/tests/bin.sh
@@ -4,3 +4,15 @@
 exit 1234
 exit hello 1234
 exit hello1234
+/bin/ls -l | wc -l
+/bin/ls | grep "some_file"
+/bin/ls /bin | grep "ls"
+/bin/ls -la | grep "\.bashrc"
+/bin/echo "Hello, world!" | grep "Hello"
+/bin/echo "Minishell test" > testfile.txt
+/bin/cat testfile.txt | grep "Minishell"
+/bin/echo "hello minishell" | /bin/grep "minishell"
+/bin/echo "word1 word2 word3" | /bin/wc -w
+/bin/ls /bin | /bin/wc -l
+
+
--- mini_tester/tests/env.sh
+++ mini_tester/tests/env.sh
@@ -1,7 +1,8 @@
 env | grep -v "_"
 export SCHOOL=42
 export PROJECT=minishell SCORE=100
-echo "welcome at $SCHOOL. You passed the project $PROJECT with a score of $SCORE"
+echo "Welcome at $SCHOOL. You passed the project $PROJECT with a score of $SCORE"
 unset SCHOOL
-echo "welcome at $SCHOOL. You passed the project $PROJECT with a score of $SCORE"
+echo "Welcome at $SCHOOL. You passed the project $PROJECT with a score of $SCORE"
 unset PROJECT SCORE
+echo "Welcome at $SCHOOL. You passed the project $PROJECT with a score of $SCORE"
